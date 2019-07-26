557f4354969c9471c16be076188084021825a033
Strackeror <rremilavergne@gmail.com>
2019-07-26 22:09:12 +0200

    Add mib encrypter/decrypter

--- generate.py
+++ generate.py
@@ -3,7 +3,9 @@ import PyInstaller.__main__
 files = [
     r"lmt_manipulator\lmt_manipulator.py",
     r"move-subspecies-script\move-subspecies.py",
-    r"quest-sobj-script\quest-sobj-script.py" 
+    r"quest-sobj-script\quest-sobj-script.py",
+    r"mib-decrypt\mib-decrypt.py",
+    
 ]
 
 for f in files:
